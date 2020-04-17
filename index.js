e98ff343cf005a7c9b043e99474cf55c05f91848
Kaleyfry <59694186+Kaleyfry@users.noreply.github.com>
2020-04-17 16:20:37 -0400

    add in request dependency

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-var moment = require('moment');
-require("moment/min/locales.min");
-moment.locale('cs');
-console.log(moment.locale()); // cs
+const request = require('request');
+let apiKey = '5093389008109e4b3678b85075afafff';
+let city = 'huntersville';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+request(url, function (err, response, body) {
+  if(err){
+    console.log('error:', error);
+  } else {
+    let weather = JSON.parse(body);
+    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+    console.log(message);
+  }
+})
